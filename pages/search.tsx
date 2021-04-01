98d7409d05dc9ddf03a4a505647e357bfe193bd2
Gandharv <gandharvkumargarg@gmail.com>
2021-04-02 00:36:56 +0530

    Update search.tsx

--- pages/search.tsx
+++ pages/search.tsx
@@ -247,7 +247,7 @@ export default function Search({
                             'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
                           }
                         >
-                          All Designers
+                          All Brands
                         </a>
                       </Link>
                     </li>
