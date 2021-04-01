98f9b5af2e0166bfa2f490731df8f14cbc273fed
Gandharv <gandharvkumargarg@gmail.com>
2021-04-02 00:34:26 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -109,16 +109,6 @@ export default function Home({
           />
         ))}
       </Marquee>
-      <Hero
-        headline="Release Details: The Yeezy BOOST 350 V2 ‘Natural'"
-        description="
-        The Yeezy BOOST 350 V2 lineup continues to grow. We recently had the
-        ‘Carbon’ iteration, and now release details have been locked in for
-        this ‘Natural’ joint. Revealed by Yeezy Mafia earlier this year, the
-        shoe was originally called ‘Abez’, which translated to ‘Tin’ in
-        Hebrew. It’s now undergone a name change, and will be referred to as
-        ‘Natural’."
-      />
       <Grid layout="B">
         {featured.slice(3, 6).map(({ node }, i) => (
           <ProductCard
