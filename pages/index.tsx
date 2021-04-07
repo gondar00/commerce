49350873977f1d58b58dc8b12c116d503070a878
Gandharv <gandharvkumargarg@gmail.com>
2021-04-08 00:44:45 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,7 +88,7 @@ export default function Home({
   <div className="carousel relative container mx-auto" style={{maxWidth: 1600}}>
     <div className="carousel-inner relative overflow-hidden w-full">
       {/*Slide 1*/}
-      <input className="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden checked="checked" />
+      <input className="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden checked />
       <div className="carousel-item absolute opacity-0" style={{height: '50vh'}}>
         <div className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style={{backgroundImage: 'url("https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80")'}}>
           <div className="container mx-auto">
